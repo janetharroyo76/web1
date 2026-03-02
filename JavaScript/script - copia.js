968fa44fcdbbb3f491bed385cc73cd5709b4c55b
janetharroyo76 <janetharroyo666@gmail.com>
2026-03-02 09:40:29 -0600

    Add files via upload

---
+++ JavaScript/script - copia.js
@@ -0,0 +1,6 @@
+//obtener el boton por su ID que se puse 
+const boton1=document.getElementById('tilin')
+//Agregar un evento al boton
+boton1.addEventListener('click', function(){
+    alert("100 de limon porque con la de jamaica tose -Irving Alejandro");
+});
